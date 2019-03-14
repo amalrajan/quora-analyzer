8e2a8bb2ebc46d3da2f17cefd4502961a9cbc580
Amal Rajan <amalrajan74@gmail.com>
2019-03-14 16:37:52 +0530

    Reformat code

--- source/scrape.py
+++ source/scrape.py
@@ -14,7 +14,8 @@ def initialize_browser(headless=False):
 
     options = Options()
     options.headless = headless
-    driver = webdriver.Firefox(options=options, executable_path="..\\browser\geckodriver.exe")
+    driver = webdriver.Firefox(
+        options=options, executable_path="..\\browser\geckodriver.exe")
 
     return driver
 
@@ -30,20 +31,24 @@ def scrape_data(driver, profile_name):
 
     driver.get("https://www.quora.com/profile/{}".format(profile_name))
 
-    answers_count = int(driver.find_element_by_xpath("//span[contains(@class, 'list_count')]").text)
+    answers_count = int(driver.find_element_by_xpath(
+        "//span[contains(@class, 'list_count')]").text)
     answer_links = []
 
     while len(answer_links) < answers_count:
         answer_links = driver.find_elements_by_class_name("question_link")
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(1)
 
-    view_more_links = driver.find_elements_by_xpath("//a[contains(@class, 'ui_qtext_more_link')]")
+    view_more_links = driver.find_elements_by_xpath(
+        "//a[contains(@class, 'ui_qtext_more_link')]")
 
     for view_more_link in view_more_links:
         view_more_link.click()
 
-    expanded_answers = driver.find_elements_by_xpath("//div[contains(@class, 'ui_qtext_expanded')]")
+    expanded_answers = driver.find_elements_by_xpath(
+        "//div[contains(@class, 'ui_qtext_expanded')]")
 
     out_file = open("answer_blob.txt", "w", encoding="utf-8")
 
